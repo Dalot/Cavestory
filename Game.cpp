9f40797c46414aa530e924e402933c525b333f85
Dalot <ge.dalot@gmail.com>
2020-11-08 18:51:24 +0000

    Added Input

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,12 @@
 #include <SDL.h>
 #include "Game.h"
 #include "Graphics.h"
+#include "Input.h"
+
+namespace {
+	const int FPS = 50;
+	const int MAX_FRAME_TIME = 5 * 1000 / FPS;
+}
 
 Game::Game() {
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -14,12 +20,37 @@ Game::~Game() {
 void Game::gameLoop() {
 	Graphics graphics;
 	SDL_Event event;
+	Input input;
+
+	int LAST_UPDATE_TIME = SDL_GetTicks();
+
+
+
 	while (true) {
+		input.beginNewFrame();
+
 		if (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
+			if (event.type == SDL_KEYDOWN) {
+				if (event.key.repeat == 0) {
+					input.keyDownEvent(event);
+				}
+			}
+			else if (event.type == SDL_KEYUP) {
+				input.keyUpEvent(event);
+			}
+			else if (event.type == SDL_QUIT) {
 				return;
 			}
 		}
+
+		if (input.wasKeyPressed(SDL_SCANCODE_ESCAPE) == true) {
+			return;
+		}
+
+		const int CURRENT_TIME_MS = SDL_GetTicks();
+		int ELAPSED_TIME_MS = CURRENT_TIME_MS - LAST_UPDATE_TIME;
+		update(std::min(ELAPSED_TIME_MS, MAX_FRAME_TIME));
+		LAST_UPDATE_TIME = CURRENT_TIME_MS;
 	}
 }
 
